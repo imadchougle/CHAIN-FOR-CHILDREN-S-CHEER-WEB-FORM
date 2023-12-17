e44fd460b6ac78d29112cea4238dbd010ec4c66a
imadchougle <imadchougle2@gmail.com>
2023-12-17 13:31:14 +0530

    created radio buttons

--- webformapp/forms.py
+++ webformapp/forms.py
@@ -1,8 +1,27 @@
+# webformapp/forms.py
 from django import forms
+from django_countries.fields import CountryField
 from .models import FormData
 
-
 class WebForm(forms.ModelForm):
+    # Radio button choices
+    RADIO_CHOICES = [
+        ('Active Ant', 'Active Ant'),
+        ('Speedy Sparrow', 'Speedy Sparrow'),
+        ('Electric Elephant', 'Electric Elephant'),
+    ]
+
+    # Add the new field for radio buttons
+    radio_buttons = forms.ChoiceField(
+        choices=RADIO_CHOICES,
+        widget=forms.RadioSelect,
+        label='Choose one',
+    )
+
     class Meta:
         model = FormData
-        fields = ['name', 'profession', 'city', 'country', 'email_or_mobile', 'remarks', 'choice_field']
+        fields = ['name', 'profession', 'city', 'country', 'email_or_mobile', 'remarks', 'radio_buttons']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['radio_buttons'].label = 'Choose one'
--- webformapp/models.py
+++ webformapp/models.py
@@ -1,26 +1,21 @@
-# webformapp/models.py
 from django.db import models
 from django_countries.fields import CountryField
 
 
 class FormData(models.Model):
-    ANT = 'Active Ant'
-    SPARROW = 'Speedy Sparrow'
-    ELEPHANT = 'Electric Elephant'
-
-    CHOICES = [
-        (ANT, 'Active Ant'),
-        (SPARROW, 'Speedy Sparrow'),
-        (ELEPHANT, 'Electric Elephant'),
+    RADIO_CHOICES = [
+        ('Active Ant', 'Active Ant'),
+        ('Speedy Sparrow', 'Speedy Sparrow'),
+        ('Electric Elephant', 'Electric Elephant'),
     ]
 
+    radio_buttons = models.CharField(max_length=50, choices=RADIO_CHOICES, blank=True, null=True)
     name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    country = CountryField(blank=True, null=True)
+    country = CountryField(blank=True, null=True, default="IN")
     email_or_mobile = models.CharField(max_length=100, help_text="Enter either email or mobile")
     remarks = models.TextField(blank=True, null=True)
-    choice_field = models.CharField(max_length=50, choices=CHOICES, default=ANT)
 
     def __str__(self):
         return self.name
