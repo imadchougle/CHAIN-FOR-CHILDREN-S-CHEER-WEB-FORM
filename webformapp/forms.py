340e727ba477dbd9d35f97d3b3c6946979a20080
imadchougle <imadchougle2@gmail.com>
2023-12-17 12:52:30 +0530

    edited forms and pages

--- webformapp/forms.py
+++ webformapp/forms.py
@@ -5,4 +5,4 @@ from .models import FormData
 class WebForm(forms.ModelForm):
     class Meta:
         model = FormData
-        fields = ['name', 'profession', 'city', 'country', 'email_or_mobile', 'remarks']
+        fields = ['name', 'profession', 'city', 'country', 'email_or_mobile', 'remarks', 'choice_field']
--- webformapp/models.py
+++ webformapp/models.py
@@ -1,13 +1,25 @@
+# webformapp/models.py
 from django.db import models
 
 
 class FormData(models.Model):
+    ANT = 'Active Ant'
+    SPARROW = 'Speedy Sparrow'
+    ELEPHANT = 'Electric Elephant'
+
+    CHOICES = [
+        (ANT, 'Active Ant'),
+        (SPARROW, 'Speedy Sparrow'),
+        (ELEPHANT, 'Electric Elephant'),
+    ]
+
     name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     email_or_mobile = models.CharField(max_length=100, help_text="Enter either email or mobile")
     remarks = models.TextField(blank=True, null=True)
+    choice_field = models.CharField(max_length=50, choices=CHOICES, default=ANT)
 
     def __str__(self):
         return self.name
