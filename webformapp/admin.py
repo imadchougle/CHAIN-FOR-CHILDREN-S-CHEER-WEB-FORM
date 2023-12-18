b5839e65d6c768342fb83c4479404ff960975497
imadchougle <imadchougle2@gmail.com>
2023-12-18 11:34:19 +0530

    admin

--- webformapp/admin.py
+++ webformapp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import FormData
 
-admin.site.register(FormData)
+
+class FormDataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'profession', 'city', 'radio_buttons', 'email_or_mobile', 'remarks')
+
+
+admin.site.register(FormData, FormDataAdmin)
